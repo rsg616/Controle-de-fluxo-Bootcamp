f70b6e3b453704ed7d4cb042bfeba074d331ae19
Nestor Garraza Sondahl <148396273+rsg616@users.noreply.github.com>
2025-05-01 22:52:02 -0300

    Add files via upload

---
+++ Contador.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Contador {
+
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = Integer.parseInt(terminal.nextLine());
+
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = Integer.parseInt(terminal.nextLine());
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+        terminal.close();
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm >= parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem = parametroDois - parametroUm;
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
+
+
+class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException() {
+        super("O segundo parâmetro deve ser maior que o primeiro");
+    }
+}
